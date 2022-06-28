77e5a6d53f2d0f1fcd4d9c17fe95055c7cf4ba86
Grahame White <4whitg16@solent.ac.uk>
2022-06-28 23:07:55 +0100

    lint

--- aigo/board.py
+++ aigo/board.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from aigo.go_string import GoString
 from aigo.types.player import Player
 from aigo.types.point import Point
@@ -12,7 +14,7 @@ class Board:
     def place_stone(self, player: Player, point: Point):
         assert self.__is_on_grid(point)
         assert self.__get(point) is None
-        liberties = []
+        liberties: List[Point] = []
         for neighbour in point.neighbours():
             if not self.__is_on_grid(neighbour):
                 continue
@@ -27,10 +29,10 @@ class Board:
     def __is_on_grid(self, point: Point) -> bool:
         return 1 <= point.row <= self.rows and 1 <= point.col <= self.cols
 
-    def __get(self, point) -> Player:
+    def __get(self, point) -> Union[Player, None]:
         string = self.__grid.get(point)
         return None if string is None else string.colour
 
-    def get_go_string(self, point: Point) -> GoString:
+    def get_go_string(self, point: Point) -> Union[GoString, None]:
         string = self.__grid.get(point)
         return None if string is None else string
