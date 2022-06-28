3343f89eed01eebf17209369d0b7a8dec5c9fea4
Grahame White <4whitg16@solent.ac.uk>
2022-06-28 18:22:37 +0100

    Added player class and tests

---
+++ aigo/tests/test_player.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from hamcrest import assert_that, equal_to
+
+from aigo.player import Player
+
+
+class TestBoard(TestCase):
+    def test_other_returns_black_when_player_is_white(self):
+        player = Player.white
+        assert_that(player.other, equal_to(Player.black))
+
+    def test_other_returns_white_when_player_is_black(self):
+        player = Player.black
+        assert_that(player.other, equal_to(Player.white))
