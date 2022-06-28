4ec6b09507d093e26c8c3a26db9e8e12400bf32b
Grahame White <4whitg16@solent.ac.uk>
2022-06-28 22:57:43 +0100

    adding place_stone logic

--- aigo/board.py
+++ aigo/board.py
@@ -1,4 +1,39 @@
+from aigo.go_string import GoString
+from aigo.types.player import Player
+from aigo.types.point import Point
+
+
 class Board:
     def __init__(self, rows, cols):
         self.cols = cols
         self.rows = rows
+        self.__grid = {}
+
+    def place_stone(self, player: Player, point: Point):
+        assert self.__is_on_grid(point)
+        assert self.__get(point) is None
+        liberties = []
+        for neighbour in point.neighbours():
+            if not self.__is_on_grid(neighbour):
+                continue
+            neighbour_string = self.__get(neighbour)
+            if neighbour_string is None:
+                liberties.append(neighbour)
+
+        new_string = GoString(player, [point], liberties)
+        for go_string_point in new_string.stones:
+            self.__grid[go_string_point] = new_string
+
+    def __is_on_grid(self, point: Point) -> bool:
+        return (
+                1 <= point.row <= self.rows and
+                1 <= point.col <= self.cols
+        )
+
+    def __get(self, point) -> Player:
+        string = self.__grid.get(point)
+        return None if string is None else string.colour
+
+    def get_go_string(self, point: Point) -> GoString:
+        string = self.__grid.get(point)
+        return None if string is None else string
--- aigo/tests/test_board.py
+++ aigo/tests/test_board.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 
 from aigo.board import Board
-
-from hamcrest import assert_that, equal_to
+from aigo.go_string import GoString
+from aigo.types.player import Player
+from aigo.types.point import Point
+from hamcrest import assert_that, calling, equal_to, instance_of, raises
 
 
 class TestBoard(TestCase):
@@ -13,3 +15,60 @@ class TestBoard(TestCase):
     def test_constructor_sets_cols_to_passed_in_size(self):
         board = Board(0, 123)
         assert_that(board.cols, equal_to(123))
+
+    def test_place_stone_throws_exception_if_point_row_greater_than_grid_row(self):
+        board = Board(5, 5)
+        assert_that(calling(board.place_stone).with_args(Player.white, Point(6, 3)), raises(Exception))
+
+    def test_place_stone_throws_exception_if_point_row_less_than_grid_row(self):
+        board = Board(5, 5)
+        assert_that(calling(board.place_stone).with_args(Player.white, Point(0, 3)), raises(Exception))
+
+    def test_place_stone_throws_exception_if_point_col_greater_than_grid_col(self):
+        board = Board(5, 5)
+        assert_that(calling(board.place_stone).with_args(Player.white, Point(3, 6)), raises(Exception))
+
+    def test_place_stone_throws_exception_if_point_col_less_than_grid_col(self):
+        board = Board(5, 5)
+        assert_that(calling(board.place_stone).with_args(Player.white, Point(3, 0)), raises(Exception))
+
+    def test_place_stone_throws_exception_if_point_already_played(self):
+        board = Board(5, 5)
+        board.place_stone(Player.white, Point(1, 1))
+        assert_that(calling(board.place_stone).with_args(Player.white, Point(1, 1)), raises(Exception))
+
+    def test_place_stone_adds_neighbours_to_liberties(self):
+        board = Board(5, 5)
+        board.place_stone(Player.white, Point(3, 3))
+        string = board.get_go_string(Point(3, 3))
+        expected_liberties = {Point(2, 3), Point(4, 3), Point(3, 2), Point(3, 4)}
+        assert_that(string.liberties, equal_to(expected_liberties))
+
+    def test_place_stone_only_adds_neighbours_on_grid_to_liberties(self):
+        board = Board(5, 5)
+        board.place_stone(Player.white, Point(3, 1))
+        string = board.get_go_string(Point(3, 1))
+        expected_liberties = {Point(2, 1), Point(4, 1), Point(3, 2)}
+        assert_that(string.liberties, equal_to(expected_liberties))
+
+    def test_place_stone_adds_unclaimed_neighbours_to_liberties(self):
+        board = Board(5, 5)
+        board.place_stone(Player.black, Point(2, 3))
+        board.place_stone(Player.white, Point(3, 3))
+        string = board.get_go_string(Point(3, 3))
+        expected_liberties = {Point(4, 3), Point(3, 2), Point(3, 4)}
+        assert_that(string.liberties, equal_to(expected_liberties))
+
+    # adding a stone next to other player's stone should remove a liberty from other player's stone
+    #  .      .
+    # .o. -> .ox
+    #  .      .
+
+    def test_get_go_string_returns_none_if_point_unclaimed(self):
+        board = Board(5, 5)
+        assert_that(board.get_go_string(Point(1, 1)), equal_to(None))
+
+    def test_get_go_string_returns_go_string_if_point_claimed(self):
+        board = Board(5, 5)
+        board.place_stone(Player.white, Point(1, 1))
+        assert_that(board.get_go_string(Point(1, 1)), instance_of(GoString))
