4c6fcdb29be75d518762be838c47ca013fda861e
Grahame White <4whitg16@solent.ac.uk>
2022-06-28 19:22:54 +0100

    Added Move class and tests

--- aigo/move.py
+++ aigo/move.py
@@ -2,7 +2,7 @@ class Move:
     def __init__(self, point=None, is_pass=False, is_resign=False):
         assert (point is not None) ^ is_pass ^ is_resign
         self.point = point
-        self.is_play = (point is not None)
+        self.is_play = point is not None
         self.is_pass = is_pass
         self.is_resign = is_resign
 
