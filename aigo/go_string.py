64bd90aab8a3d6fe3cbf5a7f0a113a7903c5b886
Grahame White <4whitg16@solent.ac.uk>
2022-06-28 20:57:18 +0100

    adding GoString class and tests

--- aigo/go_string.py
+++ aigo/go_string.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
-from typing import List
+from typing import List, Union, Set
 
 from aigo.types.player import Player
 from aigo.types.point import Point
 
 
 class GoString:
-    def __init__(self, colour: Player, stones: List[Point], liberties: List[Point]):
+    def __init__(self, colour: Player, stones: Union[List[Point], Set[Point]], liberties: Union[List[Point], Set[Point]]):
         self.colour = colour
         self.stones = set(stones)
         self.liberties = set(liberties)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+               self.colour == other.colour and \
+               self.stones == other.stones and \
+               self.liberties == other.liberties
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
 
     def remove_liberty(self, point: Point):
         self.liberties.remove(point)
@@ -18,4 +28,7 @@ class GoString:
         self.liberties.add(point)
 
     def merged_with(self, go_string: GoString) -> GoString:
-        return GoString(Player.black, [], [])
+        assert self.colour == go_string.colour
+        combined_stones = self.stones | go_string.stones
+        combined_liberties = (self.liberties | go_string.liberties) - combined_stones
+        return GoString(self.colour, combined_stones, combined_liberties)
--- aigo/tests/test_go_string.py
+++ aigo/tests/test_go_string.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from aigo.go_string import GoString
 from aigo.types.player import Player
 from aigo.types.point import Point
-from hamcrest import assert_that, equal_to, instance_of
+from hamcrest import assert_that, equal_to, instance_of, calling, raises
 
 
 class TestGoString(TestCase):
@@ -47,3 +47,76 @@ class TestGoString(TestCase):
         go_string = GoString(Player.white, [], [])
         result = go_string.merged_with(GoString(Player.white, [], []))
         assert_that(result, instance_of(GoString))
+
+    def test_merged_with_sets_colour_to_colour(self):
+        go_string = GoString(Player.white, [Point(1, 1)], [])
+        result = go_string.merged_with(GoString(Player.white, [Point(2, 2)], []))
+        assert_that(result.colour, equal_to(Player.white))
+
+    def test_merged_with_throws_exception_when_colours_different(self):
+        go_string = GoString(Player.black, [Point(1, 1)], [])
+        assert_that(calling(lambda: go_string.merged_with(GoString(Player.white, [Point(2, 2)], []))), raises(Exception))
+
+    def test_merged_with_sets_stones_to_combined_list_of_stones(self):
+        go_string = GoString(Player.white, [Point(1, 1)], [])
+        result = go_string.merged_with(GoString(Player.white, [Point(2, 2)], []))
+        assert_that(result.stones, equal_to({Point(1, 1), Point(2, 2)}))
+
+    def test_merged_with_sets_liberties_to_combined_list_of_liberties(self):
+        go_string = GoString(Player.white, [], [Point(1, 1)])
+        result = go_string.merged_with(GoString(Player.white, [], [Point(2, 2)]))
+        assert_that(result.liberties, equal_to({Point(1, 1), Point(2, 2)}))
+
+    def test_merged_with_removed_combined_stones_from_combined_list_of_liberties(self):
+        go_string = GoString(Player.white, [], [Point(1, 1)])
+        result = go_string.merged_with(GoString(Player.white, [Point(1, 1)], [Point(2, 2)]))
+        assert_that(result.liberties, equal_to({Point(2, 2)}))
+
+    def test_num_liberties_returns_liberties_in_set(self):
+        liberties = [Point(1, 2), Point(5, 5)]
+        go_string = GoString(Player.white, [], liberties)
+        assert_that(go_string.num_liberties, equal_to(2))
+
+    def test_equality_returns_true_when_other_instance_is_go_string(self):
+        go_string = GoString(Player.black, [], [])
+        other = GoString(Player.black, [], [])
+        assert_that(go_string == other, equal_to(True))
+
+    def test_equality_returns_false_when_other_instance_is_not_go_string(self):
+        go_string = GoString(Player.black, [], [])
+        other = None
+        assert_that(go_string == other, equal_to(False))
+
+    def test_equality_returns_true_when_colour_equals_other_colour(self):
+        go_string = GoString(Player.black, [], [])
+        other = GoString(Player.black, [], [])
+        assert_that(go_string == other, equal_to(True))
+
+    def test_equality_returns_false_when_colour_not_equal_to_other_colour(self):
+        go_string = GoString(Player.white, [], [])
+        other = GoString(Player.black, [], [])
+        assert_that(go_string == other, equal_to(False))
+
+    def test_equality_returns_true_when_stones_equals_other_stones(self):
+        stones = [Point(1, 1)]
+        go_string = GoString(Player.black, stones, [])
+        other = GoString(Player.black, stones, [])
+        assert_that(go_string == other, equal_to(True))
+
+    def test_equality_returns_false_when_stones_not_equal_to_other_stones(self):
+        stones = [Point(1, 1)]
+        go_string = GoString(Player.black, stones, [])
+        other = GoString(Player.black, [], [])
+        assert_that(go_string == other, equal_to(False))
+
+    def test_equality_returns_true_when_liberties_equals_other_liberties(self):
+        liberties = [Point(1, 1)]
+        go_string = GoString(Player.black, [], liberties)
+        other = GoString(Player.black, [], liberties)
+        assert_that(go_string == other, equal_to(True))
+
+    def test_equality_returns_false_when_liberties_not_equal_to_other_liberties(self):
+        liberties = [Point(1, 1)]
+        go_string = GoString(Player.black, [], liberties)
+        other = GoString(Player.black, [], [])
+        assert_that(go_string == other, equal_to(False))
